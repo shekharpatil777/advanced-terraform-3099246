49972e02291dce79e5ad0886e10e42767e88afbc
chandrashekhar Patil <patilshekhar1412@gmail.com>
2024-08-16 10:40:26 +0530

    changes for provider

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform" #replace this with your project-id
+  project = "advancedterraform-428410" #replace this with your project-id
   region  = "us-central1"
   zone    = "us-central1-a"
 }
